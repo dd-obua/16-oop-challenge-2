987c1c80afeab0066d185b4a5b92a8776f39597f
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-14 19:40:51 +0300

    Create CarCl class

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
-  
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(this.speed);
+  }
+
+  break() {
+    this.speed -= 5;
+    console.log(this.speed);
+  }
+}
